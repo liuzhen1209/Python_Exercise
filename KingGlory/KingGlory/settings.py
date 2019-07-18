7bfc56433286292b2649dbc1f25a6c7419a50744
liuzhen1209 <liu_zhen1209@126.com>
2019-07-18 11:29:44 +0800

    '优化页面以及更换数据库为mysql'

--- KingGlory/KingGlory/settings.py
+++ KingGlory/KingGlory/settings.py
@@ -78,8 +78,14 @@ WSGI_APPLICATION = 'KingGlory.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'KingGlory',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '192.168.99.100',
+        'PORT': 3306,
     }
 }
 
